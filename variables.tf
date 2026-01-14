badb2ffd08f745b46c26f67697e7610f35501910
Systemattik <34232728+Systemattik@users.noreply.github.com>
2026-01-14 14:49:02 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,28 @@
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "westus2"
+}
+
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, test, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "storage_account_name" {
+  description = "Globally unique storage account name"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Virtual machine size"
+  type        = string
+  default     = "Standard_B1ls"
+}
 
