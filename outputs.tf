15ed698e01d361da2a06191a68c4d74eeabc0f65
Systemattik <34232728+Systemattik@users.noreply.github.com>
2026-01-14 14:49:47 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
+output "resource_group_name" {
+  description = "Resource Group name"
+  value       = azurerm_resource_group.this.name
+}
+
+output "storage_account_name" {
+  description = "Storage Account name"
+  value       = azurerm_storage_account.this.name
+}
 
