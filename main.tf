e092ed3046e0e129391d281e068883340cf34cd6
Systemattik <34232728+Systemattik@users.noreply.github.com>
2026-01-14 14:49:26 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,40 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+}
+
+resource "azurerm_network_security_group" "this" {
+  name                = "nsg-${var.environment}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    name                       = "Allow-SSH-Trusted-IP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "X.X.X.X/32"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
 
